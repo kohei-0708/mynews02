459bc8ea17f85430c6212744d3bad56dd358a9c6
”安田晃平” <ko.20112135@gmail.com>
2020-04-02 13:34:57 +0900

    Laravel １５

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\Profile;
+
 class ProfileController extends Controller
 {
   public function add()
@@ -26,6 +28,16 @@ public function update()
 
 public function create(Request $request)
   {
+    $this->validate($request, Profile::$rules);
+
+          $profile = new Profile;
+          $form = $request->all();
+
+          unset($form['_token']);
+
+          $profile->fill($form);
+          $profile->save();
+
 
       return redirect('admin/profile/create');
   }
