d1f7020b987f57bb21f6843d101b2e88fd785273
”安田晃平” <ko.20112135@gmail.com>
2020-04-06 02:28:56 +0900

    Laravel17

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -16,16 +16,6 @@ class ProfileController extends Controller
 
 
 
-public function edit()
-{
-  return view('admin.profile.edit');
-}
-
-public function update()
-{
-  return redirect('admin/profile/edit');
-}
-
 public function create(Request $request)
   {
     $this->validate($request, Profile::$rules);
@@ -41,4 +31,50 @@ public function create(Request $request)
 
       return redirect('admin/profile/create');
   }
+
+  public function index(Request $request)
+  {
+      $cond_title = $request->cond_title;
+      if ($cond_title != '') {
+          // 検索されたら検索結果を取得する
+          $posts = Profile::where('title', $cond_title)->get();
+      } else {
+          // それ以外はすべてのニュースを取得する
+          $posts = Profile::all();
+      }
+      return view('admin.profile.index', ['posts' => $posts, 'cond_title' => $cond_title]);
+  }
+  public function edit(Request $request)
+  {
+
+      $profile = Profile::find($request->id);
+      if (empty($profile)) {
+        abort(404);
+      }
+      return view('admin.profile.edit', ['profile_form' => $profile]);
+  }
+
+  public function update(Request $request)
+  {
+
+      $this->validate($request, Profile::$rules);
+
+      $profile = Profile::find($request->id);
+
+      $profile_form = $request->all();
+      unset($profile_form['_token']);
+
+      // 該当するデータを上書きして保存する
+      $profile->fill($profile_form)->save();
+
+      return redirect('admin/profile');
+  }
+  public function delete(Request $request)
+    {
+        
+        $profile = profile::find($request->id);
+        // 削除する
+        $profile->delete();
+        return redirect('admin/profile/');
+    }
 }
